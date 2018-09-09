bbb6764e9211014986b43d2f333b74322ced49bb
yourusername <youremail>
2018-09-09 18:55:36 +0200

    chages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-o -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo -t beautifulhugo # if using a theme, replace with `hugo -t <YOURTHEME>`
